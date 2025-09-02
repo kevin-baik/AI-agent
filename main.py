154f68dcc91ca35a4b4e1ac0b8b0c2650e378aeb
kbaik <>
2025-09-02 14:49:23 -0700

    modify to allow prompt to be passed as CLI argv

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import sys
 import os
 from dotenv import load_dotenv
 from google import genai
@@ -7,8 +8,12 @@ def main():
     api_key = os.environ.get("GEMINI_API_KEY")
     client = genai.Client(api_key=api_key)
     
+    if len(sys.argv) < 2:
+        sys.exit(1)
+
+    prompt = sys.argv[1]
     response = client.models.generate_content(
-        model="gemini-2.0-flash-001", contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+        model="gemini-2.0-flash-001", contents=prompt
     )
     print(response.text)
     print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
