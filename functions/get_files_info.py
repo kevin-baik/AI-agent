f285796e34811509942e33df731ffd9ee240b237
kbaik <>
2025-09-03 18:52:55 -0700

    add functions/get_files_info.get_file_content

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -1,23 +1,47 @@
 import os
+from config import CHARACTER_LIMIT
 
 def get_files_info(working_directory, directory="."):
     try:
-        abs_path_of_working_directory = os.path.abspath(working_directory)
-        fullpath = os.path.join(abs_path_of_working_directory, directory)
-        abs_fullpath = os.path.abspath(fullpath)
+        abs_working_dir = os.path.abspath(working_directory)
+        target_dir = os.path.join(abs_working_dir, directory)
+        abs_target_dir = os.path.abspath(target_dir)
 
-        if not abs_fullpath.startswith(abs_path_of_working_directory):
+        if not abs_target_dir.startswith(abs_working_dir):
            return f'\tError: Cannot list "{directory}" as it is outside the permitted working directory'
 
-        if not os.path.isdir(abs_fullpath):
+        if not os.path.isdir(abs_target_dir):
             return f'\tError: "{directory}" is not a directory'
 
         content_info = []
-        for content in os.listdir(abs_fullpath):
-            content_path = os.path.join(abs_fullpath, content)
+        for content in os.listdir(abs_target_dir):
+            content_path = os.path.join(abs_target_dir, content)
             content_info.append(f"- {content}: file_size={os.path.getsize(content_path)} bytes, is_dir={os.path.isdir(content_path)}")
 
         return "\n".join(content_info)
 
     except Exception as e:
         return f"\tError: {e}"
+
+def get_file_content(working_directory, file_path):
+    abs_working_dir = os.path.abspath(working_directory)
+    target_file = os.path.join(abs_working_dir, file_path)
+    abs_target_file = os.path.abspath(target_file)
+
+    if not abs_target_file.startswith(abs_working_dir):
+       return f'Error: Cannot read "{file_path}" as it is outside the permitted working directory'
+
+    if not os.path.isfile(abs_target_file):
+        return f'Error: File not found or is not a regular file: "{file_path}"'
+
+    try:
+        with open(abs_target_file, "r") as f:
+            file_content = f.read(CHARACTER_LIMIT)
+            if os.path.getsize(abs_target_file) > CHARACTER_LIMIT:
+                file_content += f'[...File "{file_path}" truncated at 10000 characters]'
+            return file_content
+
+    except Exception as e:
+        return f"\tError: {e}"
+
+
--- tests.py
+++ tests.py
@@ -1,6 +1,8 @@
-from functions.get_files_info import get_files_info
+from functions.get_files_info import get_files_info, get_file_content
 
 def tests():
+    # get_files_info tests
+    """
     test_cases = [
         ("calculator", "."),
         ("calculator", "pkg"),
@@ -10,8 +12,21 @@ def tests():
 
     for test in test_cases:
         print(f"Result for '{test[1]}' directory")
-        #print(f"{get_files_info(*test)}")
-        print(str(get_files_info(*test)))
+        print(f"{get_files_info(*test)}")
+    """
+    
+    # get_file_content
+    test_cases = [
+        #("calculator", "lorem.txt"),
+        ("calculator", "main.py"),
+        ("calculator", "pkg/calculator.py"),
+        ("calculator", "/bin/cat"),
+        ("calculator", "pkg/does_not_exist.py")
+    ]
+
+    for test in test_cases:
+        result = get_file_content(*test)
+        print(result)
 
 if __name__ == "__main__":
     tests()
