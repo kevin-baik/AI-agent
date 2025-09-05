2784697927fc4862bc3eac7c52682ca48f2cebbf
kbaik <>
2025-09-05 12:04:58 -0700

    add functions/get_files_info.write_file

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -45,3 +45,23 @@ def get_file_content(working_directory, file_path):
         return f"\tError: {e}"
 
 
+def write_file(working_directory, file_path, content):
+    abs_working_dir = os.path.abspath(working_directory)
+    target_file = os.path.join(abs_working_dir, file_path)
+    abs_target_file = os.path.abspath(target_file)
+
+    if not abs_target_file.startswith(abs_working_dir):
+       return f'Error: Cannot read "{file_path}" as it is outside the permitted working directory'
+    
+    dir_to_file = file_path.split("/")
+    if len(dir_to_file) > 1:
+        os.makedirs(os.path.join(abs_working_dir, *dir_to_file[:-1]), exist_ok=True)
+
+    try:
+        with open(abs_target_file, "w") as f:
+            f.write(content)
+        if os.path.exists(abs_target_file):
+            return f'Successfully wrote to "{file_path}" ({len(content)} characters written)'
+    
+    except Exception as e:
+        return f"\tError: {e}"
--- tests.py
+++ tests.py
@@ -1,4 +1,4 @@
-from functions.get_files_info import get_files_info, get_file_content
+from functions.get_files_info import get_files_info, get_file_content, write_file
 
 def tests():
     # get_files_info tests
@@ -16,6 +16,7 @@ def tests():
     """
     
     # get_file_content
+    """
     test_cases = [
         #("calculator", "lorem.txt"),
         ("calculator", "main.py"),
@@ -27,6 +28,18 @@ def tests():
     for test in test_cases:
         result = get_file_content(*test)
         print(result)
+    """
 
+    # write_file tests
+    test_cases = [
+        ("calculator", "lorem.txt", "wait, this isn't lorem ipsum"),
+        ("calculator", "pkg/morelorem.txt", "lorem ipsum dolor sit amet"),
+        ("calculator", "/tmp/temp.txt", "this should not be allowed")
+    ]
+
+    for test in test_cases:
+        result = write_file(*test)
+        print(result)
+    
 if __name__ == "__main__":
     tests()
